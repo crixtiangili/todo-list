fe884abb412e5f7feab7a61d2a35c3e568e5983e
crixtiangili <ogilichristianonyedika@gmail.com>
2022-01-30 18:04:47 -0800

    first commit

--- database/connect.php
+++ database/connect.php
@@ -2,10 +2,10 @@
 
 // Establish a connection to the database
 
-$host = "localhost";
-$user = "root";
-$db_password = "";
-$db_name = "todoist"; 
+$host = "us-cdbr-east-05.cleardb.net";
+$user = "beec901f50b6f";
+$db_password = "4a29d6f1";
+$db_name = "heroku_14a740b258d0fd2"; 
 
 
 $conn = mysqli_connect($host, $user, $db_password, $db_name) or die ("could not connect");
